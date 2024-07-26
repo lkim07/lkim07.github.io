8ddadf6ae2328b680d0ad57716ccfcc1ca000844
lkim0731 <lkim0731@gmail.com>
2024-07-26 13:44:08 -0700

    hide api key

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,11 @@
 // get current user location and weather by using Weather API
 // with displaying icon
-const API_KEY = "ccb747dd045dfc361cf44a344a7083ff";
+
 
 function onGeoSuccess(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key.API_KEY}&units=metric`;
 
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
